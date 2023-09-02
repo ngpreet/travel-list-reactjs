bf70d6c18b3697c9d211edf69936e553e06cda44
ngpreet <ng.navpreet@gmail.com>
2023-09-02 09:07:32 +0530

    UI logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 function App() {
+  const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: true },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+  ];
+
   return (
     <div className="app">
       <Logo />
       <Form />
-      <PackingList />
+      <PackingList initialItems={initialItems}/>
       <Stats />
     </div>
   );
@@ -14,24 +21,53 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
-function PackingList() {
-  const initialItems = [
-    { id: 1, description: "Passports", quantity: 2, packed: true },
-    { id: 2, description: "Socks", quantity: 12, packed: false },
-  ];
-
+function PackingList({initialItems}) {
   return (
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id}/>
         ))}
       </ul>
     </div>
@@ -41,7 +77,7 @@ function PackingList() {
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
